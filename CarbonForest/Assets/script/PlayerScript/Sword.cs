5cf9e4e86527e2851ebb2ea4ffccf1c4849bfd4c
ken <heyty005@unisa.edu.au>
2019-07-20 18:22:20 +0930

    Modulize Weapon Started

---
+++ CarbonForest/Assets/script/PlayerScript/Sword.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : Weapon
+{
+    public float attack1Range = 0.4f;
+    public float attack2Range = 1;
+    public float attack3Range = 1.53f;
+
+    public int attack1Damage;
+    public int attack2Damage;
+    public int attack3Damage;
+
+    private void Start()
+    {
+        
+    }
+
+}
---
+++ CarbonForest/Assets/script/PlayerScript/Weapon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public RuntimeAnimatorController animatorController;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
